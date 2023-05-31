23b99c17dc77332e25d0138e19f689d51bdb3d16
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-05-31 00:43:42 -0300

    Primeiro Commit, Add Dependencias e Config do Swagger

---
+++ src/main/java/com/japetech/eyecrop/EyecropApplication.java
@@ -0,0 +1,13 @@
+package com.japetech.eyecrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyecropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyecropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/japetech/eyecrop/EyecropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japetech.eyecrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyecropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
