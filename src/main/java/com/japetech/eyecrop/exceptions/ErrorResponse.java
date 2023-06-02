a92451ababa7de5f67cba74af435a32e74cdd167
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-06-02 00:35:19 -0300

    Add Classe ErrorResponse para mandar mensagens de exceções no POSTMAN

---
+++ src/main/java/com/japetech/eyecrop/exceptions/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.japetech.eyecrop.exceptions;
+
+import lombok.Data;
+
+
+public class ErrorResponse {
+    private String mensagem;
+
+    public ErrorResponse(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
+
